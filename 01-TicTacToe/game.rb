232afa2a6f07b64631d43d45f4d26fbb9534601e
CarolinaCapilla <carolina.capilla@gmail.com>
2020-09-02 15:50:48 +0100

    added main logic + testing all fine + some refactoring

---
+++ 01-TicTacToe/game.rb
@@ -0,0 +1,56 @@
+require './lib/interface'
+
+class Game
+  include Interface
+  attr_accessor :player1, :player2, :board
+
+  def initialize(player1, player2, board)
+    @player1 = player1
+    @player2 = player2
+    @board = board
+  end
+
+  def play
+    show(board.positions)
+    loop do
+      if next_move(player1)
+        game_finish? ? break : 'next'
+      else
+        (return false)
+      end
+
+      if next_move(player2)
+        game_finish? ? break : 'next'
+      else
+        (return false)
+      end
+    end
+    get_input('play?') == 'y'
+  end
+
+  private
+
+  def game_finish?
+    board.full? ? (show_text('full'); true) : check_win?(player1) || check_win?(player2)
+  end
+
+  def check_win?(player)
+    board.win?(player.inputs) ? (print player.name.to_s show_text('win'); true) : false
+  end
+
+  def next_move(player)
+    loop do
+      input = get_next_move(player.name)
+      case input
+      when 'invalid'
+        next
+      when 'quit'
+        return false
+      else
+        return true if update_input(input, player, board)
+
+        next
+      end
+    end
+  end
+end
